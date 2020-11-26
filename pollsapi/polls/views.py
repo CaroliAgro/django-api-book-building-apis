735805951a85bb4590736006458665f97fb428ce
CaroliAgro <caroli.agro@gmail.com>
2020-11-26 18:31:56 -0200

    django-api

---
+++ pollsapi/polls/views.py
@@ -0,0 +1,132 @@
+from django.shortcuts import render, get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+#from django.http import JsonResponse
+from .models import Poll, Choice
+from .serializers import PollSerializer, ChoiceSerializer,VoteSerializer, UserSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate
+from rest_framework.exceptions import PermissionDenied
+from rest_framework import viewsets
+
+
+class UserCreate(generics.CreateAPIView):
+  authentication_classes = ()
+  permission_classes = ()
+  serializer_class = UserSerializer
+
+
+'''class LoginView(APIView):
+  authentication_classes = ()
+  permission_classes = ()
+
+  def post(self, request,):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    user = authenticate(username=username, password=password)
+    if user:
+      return Response({"token": user.auth_token.key})
+    else:
+      return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
+'''
+
+class PollViewSet(viewsets.ModelViewSet):
+  queryset = Poll.objects.all()
+  serializer_class = PollSerializer
+
+  def destroy(self, request, *args, **kwargs):
+    poll = Poll.objects.get(pk=self.kwargs["pk"])
+    if not request.user == poll.created_by:
+      raise PermissionDenied("You can not delete this poll.")
+    return super().destroy(request, *args, **kwargs)
+
+class PollList(generics.ListCreateAPIView):
+  queryset = Poll.objects.all()
+  serializer_class = PollSerializer
+
+class PollDetail(generics.RetrieveDestroyAPIView):
+  queryset = Poll.objects.all()
+  serializer_class = PollSerializer
+
+class ChoiceList(generics.ListCreateAPIView):
+  def get_queryset(self):
+    queryset = Choice.objects.filter(poll_id=self.kwargs["pk"])
+    return queryset
+  serializer_class = ChoiceSerializer
+
+  def post(self, request, *args, **kwargs):
+    poll = Poll.objects.get(pk=self.kwargs["pk"])
+    print(request.user)
+    if not request.user == poll.created_by:
+      raise PermissionDenied("You can not create choice for this poll.")
+    return super().post(request, *args, **kwargs)
+
+
+class CreateVote(APIView):
+  serializer_class = VoteSerializer
+
+  def post(self, request, pk, choice_pk):
+    voted_by = request.data.get("voted_by")
+    data = {'choice': choice_pk, 'poll': pk, 'voted_by': voted_by}
+    serializer = VoteSerializer(data=data)
+    if serializer.is_valid():
+      vote = serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+#class ChoiceList(generics.ListCreateAPIView):
+  #queryset = Choice.objects.all()
+  #serializer_class = ChoiceSerializer
+
+#class CreateVote(generics.CreateAPIView):
+  #serializer_class = VoteSerializer
+
+
+
+
+
+
+
+
+
+#poll_serializer = PollSerializer(data={"question": "Mojito or morango?","created_by": 1})
+
+#poll_serializer.is_valid()
+
+#poll = poll_serializer.save()
+
+'''class PollList(APIView):
+  def get(self, request):
+    polls = Poll.objects.all()[:20]
+    data = PollSerializer(polls, many=True).data
+    print(type(data))
+    return Response(data)
+
+class PollDetail(APIView):
+  def get(self, request, pk):
+    poll = get_object_or_404(Poll, pk=pk)
+    data = PollSerializer(poll).data
+    return Response(data)
+'''
+'''def polls_list(request):
+  MAX_OBJECTS = 20
+  polls = Poll.objects.all()[:MAX_OBJECTS]
+  print(polls.values("question", "created_by__email"))
+  data = {"results": list(polls.values("question", "created_by__email", "pub_date"))}
+  return JsonResponse(data)'''
+
+'''def polls_detail(request, pk):
+  poll = get_object_or_404(Poll, pk=pk)
+  data = {"results": {
+  "question": poll.question,
+  "created_by": poll.created_by.username,
+  "pub_date": poll.pub_date
+  }}
+  return JsonResponse(data)
+'''
---
+++ readme.md
@@ -0,0 +1,86 @@
+- > criar ambiente virtual python
+- > pip install Django
+- > pip install djangorestframework
+- > django-admin startproject pollsapi
+
+- Constroe as tabelas necessárias no banco de dados
+- > python manage.py migrate 
+- Criando o app
+- > python manage.py startapp polls
+ 
+- Em INSTALLED_APPS adicionar
+- 'rest_framework','polls',
+- Notique ao Django que houve mudanças no models(novos modelos criados e as mudanças devem ser aplicadas as migrações)
+
+- > python manage.py makemigrations polls
+- > python manage.py migrate
+
+- Crie o arquivo urls.py em polls urls
+- Inclua o caminho em urls.py em pollsapi
+- Registre os models em admin
+- Por equanto a api terá doia endpoints retornado o JSON format : /polls /polls/<id>/
+
+- Crie um super usuário
+
+- > python manage.py createsuperuser
+- Serializers permitem que os dados instâncias do modelo e querysets sejam convertidos em dados nativos do python para serem facilmente renderizados em Json
+
+- Acesso ao shell
+
+- > python manage.py shell
+
+Em header 
+Authorization: Token <your token>
+
+End Points:
+POST : /users/
+
+{
+"username": "Carl",
+"email": "Carl@gmail.com",
+"password": "Carl123"
+}
+
+Header: Content-Type application/json
+
+POST : /login/
+
+{
+"username": "Carl",
+"password": "Carl123"
+}
+
+return token
+
+Header: Content-Type application/json
+
+GET: /polls/
+
+Authorization Token 22555
+
+POST : /polls/
+Header: Content-Type application/json
+Authorization Token 22555
+{
+"question": "Flask",
+"created_by": "7"
+}
+
+GET : polls/7/
+
+Authorization Token 22555
+
+
+
+POST: polls/7/choices/
+
+{
+"choice_text": "Aqui",
+"poll": '7'
+}
+
+Header: Content-Type application/json
+Authorization Token 22555
+
+DELETE: polls/7/
+Authorization Token 22555
